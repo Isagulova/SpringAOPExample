56430b81e412990b65412cd3a390f82e527b1e2b
Isagulova <isagulovaburulsun05@gmail.com>
2024-12-15 16:44:25 +0600

    AOP Project

--- src/main/java/kg/isagulova/aopproject/ImplementationOfDifferentAdvice.java
+++ src/main/java/kg/isagulova/aopproject/ImplementationOfDifferentAdvice.java
@@ -4,25 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import kg.isagulova.aopproject.service.ServiceExample;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication
 // @EnableAspectJAutoProxy annotation enables support for
 // handling the components marked with @Aspect annotation.
 @EnableAspectJAutoProxy
-public class AopProjectApplication {
-		public static void main(String[] args)
-		{
-			ConfigurableApplicationContext context
-					= SpringApplication.run(
-					AopProjectApplication.class,
-					args);
+public class ImplementationOfDifferentAdvice {
+		public static void main(String[] args) {
+			ConfigurableApplicationContext context = SpringApplication.run(ImplementationOfDifferentAdvice.class, args);
 			// Fetching the object from the application
 			// context.
-			ServiceExample service
-					= context.getBean(ServiceExample.class);
+			ServiceExample service = context.getBean(ServiceExample.class);
 			// checking for an employee available in the
 			// organization
 			String employeeNumber = "emp12345";
@@ -30,30 +23,25 @@ public class AopProjectApplication {
 				service.employeeStatus(employeeNumber);
 			}
 			catch (Exception ex) {
-				System.out.println("Exception occurred.."
-						+ ex.getMessage());
+				System.out.println("Exception occurred.." + ex.getMessage());
 			}
 			// Displaying balance in the account.
 			String employeeAccountNumber = "Emp1212";
 			try {
-				service.getAccountBalance(
-						employeeAccountNumber);
+				service.getAccountBalance(employeeAccountNumber);
 			}
 			catch (Exception ex) {
-				System.out.println("Exception occurred.."
-						+ ex.getMessage());
+				System.out.println("Exception occurred.." + ex.getMessage());
 			}
 
 			// Employee has undergone some exams for promotion.
 			// Let us check that
 			int promotionExamMarks = 650;
 			try {
-				service.eligibilityForPromotion(
-						promotionExamMarks);
+				service.eligibilityForPromotion(promotionExamMarks);
 			}
 			catch (Exception ex) {
-				System.out.println("Exception occurred.."
-						+ ex.getMessage());
+				System.out.println("Exception occurred.." + ex.getMessage());
 			}
 			// Closing the context object.
 			context.close();
--- src/main/java/kg/isagulova/aopproject/service/ServiceExample.java
+++ src/main/java/kg/isagulova/aopproject/service/ServiceExample.java
@@ -1,12 +1,10 @@
-package kg.isagulova.aopproject;
+package kg.isagulova.aopproject.service;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceExample {
-    public void
-    getAccountBalance(String employeeAccountNumber)
-    {
+    public void getAccountBalance(String employeeAccountNumber) {
         System.out.println("Inside getBalance() method");
         // To mention about for a certain
         // employeeAccountNumber value
@@ -14,19 +12,15 @@ public class ServiceExample {
             System.out.println("Total balance: ......");
         }
         else {
-            System.out.println(
-                    "Sorry! wrong account number. Please give correct account number to verify");
+            System.out.println("Sorry! wrong account number. Please give correct account number to verify");
         }
     }
 
-    public String employeeStatus(String employeeNumber)
-    {
-        System.out.println(
-                "Inside checkEmployeeExistence() method");
+    public String employeeStatus(String employeeNumber) {
+        System.out.println("Inside checkEmployeeExistence() method");
         String status = null;
         if (employeeNumber.equals("emp12345")) {
-            System.out.println(employeeNumber
-                    + " is currently active");
+            System.out.println(employeeNumber + " is currently active");
             status = "active";
         }
         else {
@@ -37,19 +31,15 @@ public class ServiceExample {
         return status;
     }
 
-    public String
-    eligibilityForPromotion(int promotionExamMarks)
-    {
-        System.out.println(
-                "Inside eligibilityForPromotion() method");
+    public String eligibilityForPromotion(int promotionExamMarks) {
+        System.out.println("Inside eligibilityForPromotion() method");
         String status = null;
         if (promotionExamMarks >= 650) {
             System.out.println("Eligible for promotion..");
             status = "eligible";
         }
         else {
-            System.out.println(
-                    "Not eligible for promotion..");
+            System.out.println("Not eligible for promotion..");
             status = "not eligible";
         }
         return status;
